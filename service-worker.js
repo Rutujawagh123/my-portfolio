1e20ece08d790641c666b747bbadcb3241ba4b2a
Rutujawagh123 <rutujawagh8767@gmail.com>
2026-01-15 15:40:22 +0530

    updated code

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,40 @@
-const CACHE_NAME = "portfolio-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/offline.html",
-  "/images/profile.jpg"
+const CACHE_NAME = "rutuja-portfolio-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./resume.pdf",
+  "./images/profile.jpg",
+  "./images/favicon.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match("/offline.html"))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
+
